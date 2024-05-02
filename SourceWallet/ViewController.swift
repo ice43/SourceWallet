afa8e7b55cb984d40b7051dcbd67f8881284b3aa
bowski43 <43mngt@gmail.com>
2024-05-02 13:21:09 +0300

    Initial Commit

---
+++ SourceWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SourceWallet
+//
+//  Created by Serge Broski on 5/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
