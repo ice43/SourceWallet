49e835766ba2fe9338edaddc5f377b97a8538fbf
bowski43 <43mngt@gmail.com>
2024-05-07 05:36:54 +0300

    added "app settings" screen

---
+++ SourceWallet/ViewControllers/AppSettingsViewController/AppSettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AppSettingsViewController.swift
+//  SourceWallet
+//
+//  Created by Serge Broski on 5/6/24.
+//
+
+import UIKit
+
+final class AppSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
