cf10c956e1beb30a39a33848b55f612864efddc2
bowski43 <43mngt@gmail.com>
2024-05-07 12:09:58 +0300

    some assets has been added

---
+++ SourceWallet/ViewControllers/MainProfileViewController/MainProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainProfileViewController.swift
+//  SourceWallet
+//
+//  Created by Serge Broski on 5/7/24.
+//
+
+import UIKit
+
+final class MainProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
