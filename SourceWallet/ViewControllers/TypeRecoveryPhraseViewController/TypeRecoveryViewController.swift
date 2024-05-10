951ee794a305edec3331e2ec58f72e0c5d3d2558
Paul <pasha.makey@gmail.com>
2024-05-11 00:40:50 +0300

    TypeRecovery Directory initial setup

---
+++ SourceWallet/ViewControllers/TypeRecoveryPhraseViewController/TypeRecoveryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TypeRecoveryViewController.swift
+//  SourceWallet
+//
+//  Created by Paul Makey on 11.05.24.
+//
+
+import UIKit
+
+final class TypeRecoveryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
