efc75ca38384457a9ef2a12be827058582825755
bowski43 <43mngt@gmail.com>
2024-05-10 22:07:16 +0300

    added side menu

--- SourceWallet/Models/User.swift
+++ SourceWallet/Models/User.swift
@@ -8,16 +8,32 @@
 import Foundation
 
 struct User {
-    let balance: String
-    var fiat: String {
-        String(format: "%.2f", (Double(balance) ?? 0) * 61467.65) + " USD"
-    }
+    let seed: [String]
+    let wallets: [Wallet]
     
     static func getUser() -> User {
         let dataStore = DataStore.shared
+        let wallets = dataStore.walletsData
         
         return User(
-            balance: dataStore.balance.first ?? ""
+            seed: dataStore.seed,
+            wallets: wallets
         )
     }
+}
+
+struct Wallet {
+    let name: String
+    let balance: String
+    let transactions: [Transaction]
+    
+    var fiat: String {
+        String(format: "%.2f", (Double(balance) ?? 0) * 61467.65) + " USD"
+    }
+}
+
+struct Transaction {
+    let type: String
+    let amount: String
+    let date: String
 }
